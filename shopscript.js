50eda3f17d83dc86278df6e079b2b31fa3985797
xvolld <136752887+xvolld@users.noreply.github.com>
2024-02-06 19:23:27 +0330

    Add files via upload

---
+++ shopscript.js
@@ -0,0 +1,6 @@
+
+function toggleMenu() {
+    var mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.style.display === 'block' ? mobileMenu.style.display = 'none' : mobileMenu.style.display = 'block';
+}
+
