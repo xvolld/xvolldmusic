16576ae2c97e331133d4e971849e72788259b2a6
xvolld <136752887+xvolld@users.noreply.github.com>
2024-02-06 19:21:03 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
- document.body.style.overflow = "auto";
 
+document.body.style.overflow = "auto";
+// تابع toggleMenu() برای نمایش/مخفی کردن منوی همبرگر
+function toggleMenu() {
+    var mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.style.display === 'block' ? mobileMenu.style.display = 'none' : mobileMenu.style.display = 'block';
+}
